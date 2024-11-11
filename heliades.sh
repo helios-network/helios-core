02ca80d63b02a06af381feca818884799008f3dd
w0dm4n <w0dm4n@hotmail.fr>
2024-11-11 19:14:20 +0100

    API && GRPC added

--- heliades.sh
+++ heliades.sh
@@ -3,4 +3,6 @@ ulimit -n 120000
 yes 12345678 | heliades \
 --log-level "info" \
 --rpc.laddr "tcp://0.0.0.0:26657" \
+--api.address="tcp://0.0.0.0:1317" \
+--grpc.enable=true --grpc.address="0.0.0.0:9090" \
 start
