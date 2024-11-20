0343b032f6cd32df3896260700cb1ac8400b4c5c
jguyet <jguyet.weoohh@gmail.com>
2024-11-20 04:59:42 +0100

    protogen + update_sdk_go works

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-echo "Generating Helios proto code"
+echo "Generating gogo proto code"
 cd proto
 proto_dirs=$(find ./helios -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
@@ -14,9 +14,34 @@ done
 
 cd ..
 
+DIRECTORY_TO_OVERLOAD="helios-core"
+
+echo "Replacing strings in all files within $DIRECTORY_TO_OVERLOAD"
+
+# Vérifier que le répertoire existe
+if [[ ! -d "$DIRECTORY_TO_OVERLOAD" ]]; then
+  echo "Error: Directory $DIRECTORY_TO_OVERLOAD does not exist."
+  exit 1
+fi
+
+# Parcourir tous les fichiers du répertoire
+for file in $(find "$DIRECTORY_TO_OVERLOAD" -type f); do
+  echo "Processing file: $file"
+  
+  # Remplacer les chaînes dans chaque fichier
+  awk '{
+    gsub("github.com/gogo/protobuf/grpc", "github.com/cosmos/gogoproto/grpc");
+    gsub("github.com/gogo/protobuf/proto", "github.com/cosmos/gogoproto/proto");
+    gsub("github.com/gogo/protobuf/types", "github.com/cosmos/gogoproto/types");
+    gsub("github_com_gogo_protobuf_types", "github_com_cosmos_gogoproto_types");
+    print;
+  }' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+done
+
+echo "Replacement completed for all files in $DIRECTORY_TO_OVERLOAD."
+
 # move proto files to the right places
 cp -r helios-core/* ./
 rm -rf helios-core
 
-# Uncomment if needed
 # ./scripts/protocgen-pulsar.sh
--- scripts/update_sdk_go.sh
+++ scripts/update_sdk_go.sh
@@ -2,13 +2,112 @@
 # On Linux: change command to sed
 
 cp helios-chain/modules/auction/types/*.go ../sdk-go/chain/auction/types/
+
+# Parcourir tous les fichiers du répertoire
+for file in $(find "../sdk-go/chain/auction/types/" -type f); do
+  echo "Processing file: $file"
+  
+  # Remplacer les chaînes dans chaque fichier
+  awk '{
+    gsub("helios-core/helios-chain/modules", "github.com/Helios-Chain-Labs/sdk-go/chain");
+    gsub("helios-core/helios-chain", "github.com/Helios-Chain-Labs/sdk-go/chain");
+    print;
+  }' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+done
+
 cp helios-chain/modules/exchange/types/*.go ../sdk-go/chain/exchange/types/
+
+# Parcourir tous les fichiers du répertoire
+for file in $(find "../sdk-go/chain/exchange/types/" -type f); do
+  echo "Processing file: $file"
+  
+  # Remplacer les chaînes dans chaque fichier
+  awk '{
+    gsub("helios-core/helios-chain/modules", "github.com/Helios-Chain-Labs/sdk-go/chain");
+    gsub("helios-core/helios-chain", "github.com/Helios-Chain-Labs/sdk-go/chain");
+    print;
+  }' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+done
+
 cp helios-chain/modules/ocr/types/*.go ../sdk-go/chain/ocr/types/
+
+# Parcourir tous les fichiers du répertoire
+for file in $(find "../sdk-go/chain/ocr/types/" -type f); do
+  echo "Processing file: $file"
+  
+  # Remplacer les chaînes dans chaque fichier
+  awk '{
+    gsub("helios-core/helios-chain/modules", "github.com/Helios-Chain-Labs/sdk-go/chain");
+    gsub("helios-core/helios-chain", "github.com/Helios-Chain-Labs/sdk-go/chain");
+    print;
+  }' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+done
+
 cp helios-chain/modules/peggy/types/*.go ../sdk-go/chain/peggy/types/
+# Parcourir tous les fichiers du répertoire
+for file in $(find "../sdk-go/chain/peggy/types/" -type f); do
+  echo "Processing file: $file"
+  
+  # Remplacer les chaînes dans chaque fichier
+  awk '{
+    gsub("helios-core/helios-chain/modules", "github.com/Helios-Chain-Labs/sdk-go/chain");
+    gsub("helios-core/helios-chain", "github.com/Helios-Chain-Labs/sdk-go/chain");
+    print;
+  }' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+done
+
 cp helios-chain/modules/wasmx/types/*.go ../sdk-go/chain/wasmx/types/
+# Parcourir tous les fichiers du répertoire
+for file in $(find "../sdk-go/chain/wasmx/types/" -type f); do
+  echo "Processing file: $file"
+  
+  # Remplacer les chaînes dans chaque fichier
+  awk '{
+    gsub("helios-core/helios-chain/modules", "github.com/Helios-Chain-Labs/sdk-go/chain");
+    gsub("helios-core/helios-chain", "github.com/Helios-Chain-Labs/sdk-go/chain");
+    print;
+  }' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+done
+
 cp helios-chain/modules/insurance/types/*.go ../sdk-go/chain/insurance/types/
+# Parcourir tous les fichiers du répertoire
+for file in $(find "../sdk-go/chain/insurance/types/" -type f); do
+  echo "Processing file: $file"
+  
+  # Remplacer les chaînes dans chaque fichier
+  awk '{
+    gsub("helios-core/helios-chain/modules", "github.com/Helios-Chain-Labs/sdk-go/chain");
+    gsub("helios-core/helios-chain", "github.com/Helios-Chain-Labs/sdk-go/chain");
+    print;
+  }' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+done
+
 cp helios-chain/modules/oracle/types/*.go ../sdk-go/chain/oracle/types/
+# Parcourir tous les fichiers du répertoire
+for file in $(find "../sdk-go/chain/oracle/types/" -type f); do
+  echo "Processing file: $file"
+  
+  # Remplacer les chaînes dans chaque fichier
+  awk '{
+    gsub("helios-core/helios-chain/modules", "github.com/Helios-Chain-Labs/sdk-go/chain");
+    gsub("helios-core/helios-chain", "github.com/Helios-Chain-Labs/sdk-go/chain");
+    print;
+  }' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+done
+
 cp helios-chain/modules/tokenfactory/types/*.go ../sdk-go/chain/tokenfactory/types/
+# Parcourir tous les fichiers du répertoire
+for file in $(find "../sdk-go/chain/tokenfactory/types/" -type f); do
+  echo "Processing file: $file"
+  
+  # Remplacer les chaînes dans chaque fichier
+  awk '{
+    gsub("helios-core/helios-chain/modules", "github.com/Helios-Chain-Labs/sdk-go/chain");
+    gsub("helios-core/helios-chain", "github.com/Helios-Chain-Labs/sdk-go/chain");
+    print;
+  }' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+done
+
 cp -r proto/ ../sdk-go/proto
 
 
