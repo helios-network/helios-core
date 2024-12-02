a1b19b7cd08357cb8d3910dca32341eda72258ad
w0dm4n <w0dm4n@hotmail.fr>
2024-12-02 21:36:02 +0100

    add cli option in build folder

--- cmd/heliades/heliades.sh
+++ cmd/heliades/heliades.sh
@@ -4,7 +4,14 @@ ulimit -n 120000
 --chain-id 4242 \
 --log_level "info" \
 --rpc.laddr "tcp://0.0.0.0:26657" \
---minimum-gas-prices "0.1ahelios" \
---grpc.enable=true --grpc.address="0.0.0.0:9090" \
---json-rpc.api eth,txpool,personal,net,debug,web3 \
---json-rpc.address="0.0.0.0:8545"
+--minimum-gas-prices "0.1helios" \
+--grpc.enable=true \
+--grpc.address="0.0.0.0:9090" \
+--grpc-web.enable=true \
+--api.enable=true \
+--api.enabled-unsafe-cors=true \
+--json-rpc.enable=true \
+--json-rpc.api "eth,txpool,personal,net,debug,web3" \
+--json-rpc.address "0.0.0.0:8545" \
+--json-rpc.ws-address "0.0.0.0:8546" \
+--p2p.laddr "tcp://0.0.0.0:26656"
