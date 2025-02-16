3b00aca174349b374016a0ec063568aa53637759
w0dm4n <w0dm4n@hotmail.fr>
2025-02-16 22:47:52 +0100

    network update

--- helios-chain/contracts/hardhat.config.js
+++ helios-chain/contracts/hardhat.config.js
@@ -1,17 +1,20 @@
+require("@nomicfoundation/hardhat-ethers"); // For latest Hardhat versions
+// require("@nomiclabs/hardhat-ethers"); // Use this for older Hardhat versions
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
     compilers: [
-      {
-        version: "0.8.20",
-      },
-      // This version is required to compile the werc9 contract.
-      {
-        version: "0.4.22",
-      },
+      { version: "0.8.20" },
+      { version: "0.4.22" },
     ],
   },
   paths: {
     sources: "./solidity",
+  },
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
   },
 };
