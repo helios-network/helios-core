a5130bf3dd6db46d5621ef74527ac04c3a986bd6
jguyet <jguyet.weoohh@gmail.com>
2025-03-14 10:29:30 +0100

    Optimise GetAccountTransactionsByPageAndSize

--- helios-chain/rpc/backend/account_info.go
+++ helios-chain/rpc/backend/account_info.go
@@ -8,6 +8,7 @@ import (
 	errorsmod "cosmossdk.io/errors"
 
 	rpctypes "helios-core/helios-chain/rpc/types"
+	chronostypes "helios-core/helios-chain/x/chronos/types"
 	evmtypes "helios-core/helios-chain/x/evm/types"
 
 	sdkmath "cosmossdk.io/math"
@@ -17,6 +18,7 @@ import (
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/pkg/errors"
 
 	erc20types "helios-core/helios-chain/x/erc20/types"
@@ -301,191 +303,150 @@ func (b *Backend) GetTransactionCount(address common.Address, blockNum rpctypes.
 	return &n, nil
 }
 
+func convertChronosRPCTransactionToRPCTransaction(tx *chronostypes.CronTransactionRPC) *rpctypes.RPCTransaction {
+	hash := common.HexToHash(tx.BlockHash)
+	blockNumber := hexutil.Big(*hexutil.MustDecodeBig(tx.BlockNumber))
+	gasPrice := hexutil.Big(*hexutil.MustDecodeBig(tx.GasPrice))
+	gasFeeCap := hexutil.Big(*big.NewInt(0))
+	gasTipCap := hexutil.Big(*big.NewInt(0))
+	to := common.HexToAddress(tx.To)
+	txIndex := hexutil.Uint64(hexutil.MustDecodeUint64(tx.TransactionIndex))
+	chainId := hexutil.Big(*hexutil.MustDecodeBig(tx.ChainId))
+	value := hexutil.Big(*hexutil.MustDecodeBig(tx.Value))
+	v := hexutil.Big(*big.NewInt(0))
+	r := hexutil.Big(*big.NewInt(0))
+	s := hexutil.Big(*big.NewInt(0))
+
+	return &rpctypes.RPCTransaction{
+		BlockHash:        &hash,                                              // Convert string to common.Hash
+		BlockNumber:      &blockNumber,                                       // Convert string to hexutil.Big
+		From:             common.HexToAddress(tx.From),                       // Convert string to common.Address
+		Gas:              hexutil.Uint64(hexutil.MustDecodeUint64(tx.Gas)),   // Convert string to hexutil.Uint64
+		GasPrice:         &gasPrice,                                          // Convert string to hexutil.Big
+		GasFeeCap:        &gasFeeCap,                                         // Convert string to hexutil.Big
+		GasTipCap:        &gasTipCap,                                         // Convert string to hexutil.Big
+		Hash:             common.HexToHash(tx.Hash),                          // Convert string to common.Hash
+		Input:            hexutil.Bytes(tx.Input),                            // Convert string to hexutil.Bytes
+		Nonce:            hexutil.Uint64(hexutil.MustDecodeUint64(tx.Nonce)), // Convert string to hexutil.Uint64
+		To:               &to,                                                // Convert string to common.Address
+		TransactionIndex: &txIndex,                                           // Convert string to hexutil.Uint64
+		Value:            &value,                                             // Convert string to hexutil.Big
+		Type:             hexutil.Uint64(hexutil.MustDecodeUint64(tx.Type)),  // Convert string to hexutil.Uint64
+		Accesses:         &ethtypes.AccessList{},                             // Assuming you handle this appropriately
+		ChainID:          &chainId,                                           // Convert string to hexutil.Big
+		V:                &v,                                                 // Convert string to hexutil.Big
+		R:                &r,                                                 // Convert string to hexutil.Big
+		S:                &s,                                                 // Convert string
+	}
+}
+
 // GetAccountTransactionsByPageAndSize returns the transactions at the given page and size for the filtered by address
 func (b *Backend) GetAccountTransactionsByPageAndSize(address common.Address, page hexutil.Uint64, size hexutil.Uint64) ([]*rpctypes.RPCTransaction, error) {
-	if page == 0 || size == 0 {
-		return nil, errors.New("page and size must be greater than 0")
+	if page == 0 || size == 0 || size > 100 {
+		return nil, errors.New("invalid page or size parameters")
 	}
 
-	if size > 100 {
-		return nil, errors.New("size must be less than 100")
+	// Transactions accumulator
+	transactions := make([]*rpctypes.RPCTransaction, 0, size)
+
+	// Check for Chronos wallet transactions first
+	if _, err := b.GetCronByAddress(address); err == nil {
+		chronTxs, err := b.GetCronTransactionsByPageAndSize(page, size)
+		if err != nil {
+			return nil, err
+		}
+		for _, tx := range chronTxs {
+			transactions = append(transactions, convertChronosRPCTransactionToRPCTransaction(tx))
+		}
+		return transactions, nil
 	}
 
-	sizeNum := uint64(size)
-	pageNum := uint64(page)
+	// utilities
+	counter := uint64(0)
+	skip := (uint64(page) - 1) * uint64(size)
 
-	transactions := make([]*rpctypes.RPCTransaction, 0, sizeNum)
+	// Handle pending transactions first
+	pendingTxs, _ := b.PendingTransactions()
+	for _, tx := range pendingTxs {
+		msg, err := evmtypes.UnwrapEthereumMsgTx(tx)
+		if err != nil {
+			continue
+		}
+		rpctx, err := rpctypes.NewTransactionFromMsg(msg, common.Hash{}, 0, 0, nil, b.chainID)
+		if err != nil {
+			continue
+		}
+		if rpctx.From == address || (rpctx.To != nil && *rpctx.To == address) {
+			if counter >= skip {
+				transactions = append(transactions, rpctx)
+				if uint64(len(transactions)) >= uint64(size) {
+					return transactions, nil
+				}
+			}
+			counter++
+		}
+	}
 
-	pendingTxs, err := b.PendingTransactions()
+	// Fetch relevant blocks heights once
+	heightsWhereFindingTx := make([]int64, 0)
+	batchSize := int64(1000)
+	latestBlockNumber, err := b.BlockNumber()
 	if err != nil {
 		return nil, err
 	}
-
-	pageOffset := (pageNum - 1) * sizeNum
-
-	pendingTxCount := uint64(len(pendingTxs))
-
-	// add mempool txs if we are in the page range
-	if pageOffset < pendingTxCount {
-		pendingEndPosition := min(pageOffset+sizeNum, pendingTxCount)
-
-		for i := pageOffset; i < pendingEndPosition; i++ {
-			msg, err := evmtypes.UnwrapEthereumMsgTx(pendingTxs[i])
-			if err != nil {
-				return nil, fmt.Errorf("failed to unwrap ethereum tx: %w", err)
-			}
-
-			if !isAddressInvolved(msg, address) {
-				continue
-			}
-
-			rpctx, err := rpctypes.NewTransactionFromMsg(
-				msg,
-				common.Hash{},
-				uint64(0),
-				uint64(0),
-				nil,
-				b.chainID,
-			)
-			if err != nil {
-				return nil, fmt.Errorf("failed to create transaction from msg: %w", err)
-			}
-
-			transactions = append(transactions, rpctx)
-
-			if uint64(len(transactions)) >= sizeNum {
-				return transactions, nil
-			}
-		}
-	}
-
-	// we need to find txs in confirmed blocks
-	latestBlockNumber, err := b.BlockNumber()
-	if err != nil {
-		return nil, fmt.Errorf("failed to get latest block number: %w", err)
-	}
-
-	// calculate the updated page offset
-	updatedPageOffset := pageOffset - uint64(len(transactions))
-
-	// use BlockchainInfo to estimate the target block based on num_txs using batches of 100
-	const batchSize = int64(100)
 	maxHeight := int64(latestBlockNumber)
-	var targetBlock int64 = -1
-	txCount := uint64(0)
+	totalMatchedTxs := uint64(0)
 
-	// find target block to start searching for txs and filter by address
-	for maxHeight > 0 {
-		minHeight := int64(1)
-		if maxHeight > int64(batchSize) {
-			minHeight = maxHeight - int64(batchSize) + 1
+	for maxHeight > 0 && totalMatchedTxs < skip+uint64(size) {
+		minHeight := maxHeight - batchSize + 1
+		if minHeight < 1 {
+			minHeight = 1
 		}
 
-		info, err := b.clientCtx.Client.BlockchainInfo(b.ctx, int64(minHeight), int64(maxHeight))
+		info, err := b.clientCtx.Client.BlockchainLocateTxsInfo(b.ctx, minHeight, maxHeight, address.String(), "")
 		if err != nil {
 			return nil, fmt.Errorf("failed to get block metadata: %w", err)
 		}
-		for i := 0; i < len(info.BlockMetas); i++ {
-			txCount += uint64(info.BlockMetas[i].NumTxs)
-			if txCount >= updatedPageOffset {
-				targetBlock = info.BlockMetas[i].Header.Height
-				break
+
+		for _, blockMeta := range info.BlockMetas {
+			if blockMeta.NumTxs > 0 {
+				heightsWhereFindingTx = append(heightsWhereFindingTx, blockMeta.Header.Height)
+				totalMatchedTxs += uint64(blockMeta.NumTxs) // Compteur explicite ici
 			}
 		}
-		if targetBlock != -1 {
-			break
-		}
-		if maxHeight > int64(batchSize) {
-			maxHeight -= int64(batchSize)
-		} else {
+		maxHeight = minHeight - 1
+		if minHeight == 1 {
 			break
 		}
 	}
 
-	if targetBlock == -1 {
-		return nil, fmt.Errorf("txs not found for page %d and size %d", pageNum, sizeNum)
-	}
-
-	// search for txs filtered by address starting from the target block in reverse order
-	for blockHeight := targetBlock; blockHeight > 0 && uint64(len(transactions)) < sizeNum; blockHeight-- {
-
-		block, err := b.rpcClient.Block(b.ctx, &blockHeight)
+	// Reverse iterate relevant heights to maintain asc order
+	for i := 0; i < len(heightsWhereFindingTx) && uint64(len(transactions)) < uint64(size); i++ {
+		blockHeight := heightsWhereFindingTx[i]
+		block, err := b.GetBlockByNumber(rpctypes.BlockNumber(blockHeight), true)
 		if err != nil {
-			return nil, fmt.Errorf("failed to get block %d: %w", blockHeight, err)
+			continue
 		}
-
-		blockRes, err := b.rpcClient.BlockResults(b.ctx, &blockHeight)
-		if err != nil {
-			return nil, fmt.Errorf("failed to get block results for block %d: %w", blockHeight, err)
+		txs, ok := block["transactions"].([]interface{})
+		if !ok {
+			continue
 		}
-
-		// pre filter messages by address before creating full RPC tx objects
-		ethMsgs := b.EthMsgsFromTendermintBlock(block, blockRes)
-		filteredMsgs := make([]*evmtypes.MsgEthereumTx, 0)
-		for _, msg := range ethMsgs {
-			if isAddressInvolved(msg, address) {
-				filteredMsgs = append(filteredMsgs, msg)
+		for _, tx := range txs {
+			ethTx, ok := tx.(*rpctypes.RPCTransaction)
+			if !ok {
+				continue
 			}
-		}
-
-		// only fetch base fee if we have txs filtered by address
-		if len(filteredMsgs) > 0 {
-			baseFee, err := b.BaseFee(blockRes)
-			if err != nil {
-				return nil, fmt.Errorf("failed to fetch Base Fee from prunned block. Check node prunning configuration: %w", err)
-			}
-
-			// now create full RPC tx objects
-			for i, msg := range filteredMsgs {
-				rpctx, err := rpctypes.NewTransactionFromMsg(
-					msg,
-					common.BytesToHash(block.Block.Hash()),
-					uint64(block.Block.Height),
-					uint64(i),
-					baseFee,
-					b.chainID,
-				)
-				if err != nil {
-					return nil, fmt.Errorf("failed to create transaction from msg: %w", err)
+			if ethTx.From == address || (ethTx.To != nil && *ethTx.To == address) {
+				if counter >= skip {
+					transactions = append(transactions, ethTx)
+					if uint64(len(transactions)) >= uint64(size) {
+						return transactions, nil
+					}
 				}
-				transactions = append(transactions, rpctx)
-				if uint64(len(transactions)) >= sizeNum {
-					break
-				}
+				counter++
 			}
 		}
 	}
+
 	return transactions, nil
-}
-
-// isAddressInvolved is a helper method that checks if an address is involved in a transaction as sender or receiver
-func isAddressInvolved(msg *evmtypes.MsgEthereumTx, address common.Address) bool {
-	// Check sender (From)
-	if common.HexToAddress(msg.From) == address {
-		return true
-	}
-
-	// Unpack the transaction data
-	txData, err := evmtypes.UnpackTxData(msg.Data)
-	if err != nil {
-		return false
-	}
-
-	// Different transaction types have different ways to access the recipient
-	switch txData := txData.(type) {
-	case *evmtypes.LegacyTx:
-		if txData.To != "" && common.HexToAddress(txData.To) == address {
-			return true
-		}
-	case *evmtypes.AccessListTx:
-		if txData.To != "" && common.HexToAddress(txData.To) == address {
-			return true
-		}
-	case *evmtypes.DynamicFeeTx:
-		if txData.To != "" && common.HexToAddress(txData.To) == address {
-			return true
-		}
-	}
-
-	return false
 }
