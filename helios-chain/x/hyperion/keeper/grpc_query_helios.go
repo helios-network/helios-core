8596034ec8cbc56be00cf25bf440e8a50062a3be
jguyet <jguyet.weoohh@gmail.com>
2025-03-27 23:08:11 +0100

    classing messages by consumers

---
+++ helios-chain/x/hyperion/keeper/grpc_query_helios.go
@@ -0,0 +1,258 @@
+package keeper
+
+import (
+	"context"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/common"
+
+	"cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/Helios-Chain-Labs/metrics"
+
+	"helios-core/helios-chain/x/hyperion/types"
+)
+
+// [Not Used In Hyperion] ValsetConfirm queries the ValsetConfirm of the hyperion module
+func (k *Keeper) ValsetConfirm(c context.Context, req *types.QueryValsetConfirmRequest) (*types.QueryValsetConfirmResponse, error) {
+	c, doneFn := metrics.ReportFuncCallAndTimingCtx(c, k.grpcTags)
+	defer doneFn()
+
+	addr, err := sdk.AccAddressFromBech32(req.Address)
+	if err != nil {
+		return nil, errors.Wrap(sdkerrors.ErrInvalidRequest, "address invalid")
+	}
+
+	return &types.QueryValsetConfirmResponse{Confirm: k.GetValsetConfirm(sdk.UnwrapSDKContext(c), req.HyperionId, req.Nonce, addr)}, nil
+}
+
+// [Not Used In Hyperion] BatchRequestByNonce queries the BatchRequestByNonce of the hyperion module
+func (k *Keeper) BatchRequestByNonce(c context.Context, req *types.QueryBatchRequestByNonceRequest) (*types.QueryBatchRequestByNonceResponse, error) {
+	c, doneFn := metrics.ReportFuncCallAndTimingCtx(c, k.grpcTags)
+	defer doneFn()
+
+	if err := types.ValidateEthAddress(req.ContractAddress); err != nil {
+		metrics.ReportFuncError(k.svcTags)
+		return nil, errors.Wrap(sdkerrors.ErrUnknownRequest, err.Error())
+	}
+
+	foundBatch := k.GetOutgoingTXBatch(sdk.UnwrapSDKContext(c), common.HexToAddress(req.ContractAddress), req.Nonce, req.HyperionId)
+	if foundBatch == nil {
+		metrics.ReportFuncError(k.svcTags)
+		return nil, errors.Wrap(sdkerrors.ErrUnknownRequest, "Can not find tx batch")
+	}
+
+	return &types.QueryBatchRequestByNonceResponse{Batch: foundBatch}, nil
+}
+
+// [Not Used In Hyperion] DenomToERC20 queries the Cosmos Denom that maps to an Ethereum ERC20
+func (k *Keeper) DenomToERC20(c context.Context, req *types.QueryDenomToERC20Request) (*types.QueryDenomToERC20Response, error) {
+	c, doneFn := metrics.ReportFuncCallAndTimingCtx(c, k.grpcTags)
+	defer doneFn()
+
+	ctx := sdk.UnwrapSDKContext(c)
+	cosmosOriginated, erc20, err := k.DenomToERC20Lookup(ctx, req.Denom, req.HyperionId)
+
+	var ret types.QueryDenomToERC20Response
+	ret.Erc20 = erc20.Hex()
+	ret.CosmosOriginated = cosmosOriginated
+
+	return &ret, err
+}
+
+// [Not Used In Hyperion] ERC20ToDenom queries the ERC20 contract that maps to an Ethereum ERC20 if any
+func (k *Keeper) ERC20ToDenom(c context.Context, req *types.QueryERC20ToDenomRequest) (*types.QueryERC20ToDenomResponse, error) {
+	c, doneFn := metrics.ReportFuncCallAndTimingCtx(c, k.grpcTags)
+	defer doneFn()
+
+	ctx := sdk.UnwrapSDKContext(c)
+	cosmosOriginated, name := k.ERC20ToDenomLookup(ctx, common.HexToAddress(req.Erc20), req.HyperionId)
+
+	var ret types.QueryERC20ToDenomResponse
+	ret.Denom = name
+	ret.CosmosOriginated = cosmosOriginated
+
+	return &ret, nil
+}
+
+// [Not Used In Hyperion]
+func (k *Keeper) GetDelegateKeyByValidator(c context.Context, req *types.QueryDelegateKeysByValidatorAddress) (*types.QueryDelegateKeysByValidatorAddressResponse, error) {
+	c, doneFn := metrics.ReportFuncCallAndTimingCtx(c, k.grpcTags)
+	defer doneFn()
+
+	ctx := sdk.UnwrapSDKContext(c)
+
+	valAddress, err := sdk.ValAddressFromBech32(req.ValidatorAddress)
+	if err != nil {
+		metrics.ReportFuncError(k.svcTags)
+		return nil, err
+	}
+
+	valAccountAddr := sdk.AccAddress(valAddress.Bytes())
+	keys := k.GetOrchestratorAddresses(ctx, req.HyperionId)
+
+	for _, key := range keys {
+		senderAddr, err := sdk.AccAddressFromBech32(key.Sender)
+		if err != nil {
+			metrics.ReportFuncError(k.svcTags)
+			return nil, err
+		}
+		if valAccountAddr.Equals(senderAddr) {
+			return &types.QueryDelegateKeysByValidatorAddressResponse{EthAddress: key.EthAddress, OrchestratorAddress: key.Orchestrator}, nil
+		}
+	}
+
+	metrics.ReportFuncError(k.svcTags)
+	return nil, errors.Wrap(types.ErrInvalid, "No validator")
+}
+
+// [Not Used In Hyperion]
+func (k *Keeper) GetDelegateKeyByOrchestrator(c context.Context, req *types.QueryDelegateKeysByOrchestratorAddress) (*types.QueryDelegateKeysByOrchestratorAddressResponse, error) {
+	c, doneFn := metrics.ReportFuncCallAndTimingCtx(c, k.grpcTags)
+	defer doneFn()
+
+	ctx := sdk.UnwrapSDKContext(c)
+	keys := k.GetOrchestratorAddresses(ctx, req.HyperionId)
+
+	_, err := sdk.AccAddressFromBech32(req.OrchestratorAddress)
+	if err != nil {
+		metrics.ReportFuncError(k.svcTags)
+		return nil, err
+	}
+
+	for _, key := range keys {
+		if req.OrchestratorAddress == key.Orchestrator {
+			return &types.QueryDelegateKeysByOrchestratorAddressResponse{ValidatorAddress: key.Sender, EthAddress: key.EthAddress}, nil
+		}
+	}
+
+	metrics.ReportFuncError(k.svcTags)
+	return nil, errors.Wrap(types.ErrInvalid, "No validator")
+}
+
+// [Not Used In Hyperion]
+func (k *Keeper) GetPendingSendToChain(c context.Context, req *types.QueryPendingSendToChain) (*types.QueryPendingSendToChainResponse, error) {
+	c, doneFn := metrics.ReportFuncCallAndTimingCtx(c, k.grpcTags)
+	defer doneFn()
+
+	ctx := sdk.UnwrapSDKContext(c)
+	batches := k.GetOutgoingTxBatches(ctx, req.HyperionId)
+	unbatchedTx := k.GetPoolTransactions(ctx, req.HyperionId)
+	senderAddress := req.SenderAddress
+
+	res := &types.QueryPendingSendToChainResponse{}
+	res.TransfersInBatches = make([]*types.OutgoingTransferTx, 0)
+	res.UnbatchedTransfers = make([]*types.OutgoingTransferTx, 0)
+
+	for _, batch := range batches {
+		for _, tx := range batch.Transactions {
+			if tx.Sender == senderAddress {
+				res.TransfersInBatches = append(res.TransfersInBatches, tx)
+			}
+		}
+	}
+
+	for _, tx := range unbatchedTx {
+		if strings.EqualFold(tx.Sender, senderAddress) {
+			res.UnbatchedTransfers = append(res.UnbatchedTransfers, tx)
+
+		}
+	}
+
+	return res, nil
+}
+
+// [Not Used In Hyperion]
+func (k *Keeper) GetAllPendingSendToChain(c context.Context, req *types.QueryAllPendingSendToChainRequest) (*types.QueryAllPendingSendToChainResponse, error) {
+	c, doneFn := metrics.ReportFuncCallAndTimingCtx(c, k.grpcTags)
+	defer doneFn()
+
+	ctx := sdk.UnwrapSDKContext(c)
+	batches := k.GetAllOutgoingTxBatches(ctx)
+	unbatchedTx := k.GetAllPoolTransactions(ctx)
+
+	res := &types.QueryAllPendingSendToChainResponse{}
+	res.TransfersInBatches = make([]*types.OutgoingTransferTx, 0)
+	res.UnbatchedTransfers = make([]*types.OutgoingTransferTx, 0)
+
+	for _, batch := range batches {
+		res.TransfersInBatches = append(res.TransfersInBatches, batch.Transactions...)
+	}
+	res.UnbatchedTransfers = append(res.UnbatchedTransfers, unbatchedTx...)
+
+	return res, nil
+}
+
+// [Not Used In Hyperion]
+func (k *Keeper) HyperionModuleState(c context.Context, req *types.QueryModuleStateRequest) (*types.QueryModuleStateResponse, error) {
+	c, doneFn := metrics.ReportFuncCallAndTimingCtx(c, k.grpcTags)
+	defer doneFn()
+
+	ctx := sdk.UnwrapSDKContext(c)
+	state := ExportGenesis(ctx, *k)
+
+	res := &types.QueryModuleStateResponse{
+		State: &state,
+	}
+
+	return res, nil
+}
+
+// [Not Used In Hyperion]
+func (k *Keeper) MissingHyperionNonces(c context.Context, req *types.MissingNoncesRequest) (*types.MissingNoncesResponse, error) {
+	ctx := sdk.UnwrapSDKContext(c)
+	var res []string
+
+	bondedValidators, err := k.StakingKeeper.GetBondedValidatorsByPower(ctx)
+	if err != nil {
+		return nil, err
+	}
+	for i := range bondedValidators {
+		val, _ := sdk.ValAddressFromBech32(bondedValidators[i].GetOperator())
+
+		ev := k.GetLastEventByValidatorAndHyperionId(ctx, req.HyperionId, val)
+		if ev.EthereumEventNonce == 0 && ev.EthereumEventHeight == 0 {
+			res = append(res, bondedValidators[i].GetOperator())
+		}
+	}
+
+	return &types.MissingNoncesResponse{OperatorAddresses: res}, nil
+}
+
+// [Not Used In Hyperion]
+func (k *Keeper) GetHyperionIdFromChainId(c context.Context, req *types.QueryGetHyperionIdFromChainIdRequest) (*types.QueryGetHyperionIdFromChainIdResponse, error) {
+	ctx := sdk.UnwrapSDKContext(c)
+	params := k.GetParams(ctx)
+
+	for _, counterpartyChainParam := range params.CounterpartyChainParams {
+		if counterpartyChainParam.BridgeChainId == req.ChainId {
+			return &types.QueryGetHyperionIdFromChainIdResponse{
+				HyperionId: counterpartyChainParam.HyperionId,
+			}, nil
+		}
+	}
+
+	return nil, errors.Wrap(types.ErrDuplicate, "BridgeChainId not found")
+}
+
+// [Not Used In Hyperion]
+func (k Keeper) Attestation(c context.Context, req *types.QueryAttestationRequest) (*types.QueryAttestationResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "empty request")
+	}
+
+	ctx := sdk.UnwrapSDKContext(c)
+
+	att := k.GetAttestation(ctx, req.HyperionId, req.Nonce, req.ClaimHash)
+	if att == nil {
+		return nil, status.Error(codes.NotFound, "attestation not found")
+	}
+
+	return &types.QueryAttestationResponse{
+		Attestation: att,
+	}, nil
+}
---
+++ helios-chain/x/hyperion/keeper/msg_server_helios.go
@@ -0,0 +1,269 @@
+package keeper
+
+import (
+	"context"
+	"fmt"
+
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+
+	"cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/ethereum/go-ethereum/common"
+
+	"github.com/Helios-Chain-Labs/metrics"
+
+	"helios-core/helios-chain/x/hyperion/types"
+)
+
+// [Not Used In Hyperion] SetOrchestratorAddresses handles the setting of orchestrator and Ethereum addresses for a validator.
+// -------------
+// MsgSetOrchestratorAddresses
+// This function ensures that the validator exists and that the orchestrator and Ethereum addresses are not already set.
+// If the validator is valid and bonded and the addresses are not already associated with another validator, it sets the orchestrator
+// and Ethereum addresses for the given validator. It then emits an event with the new addresses to signal the successful
+// update of the orchestrator and Ethereum addresses.
+// -------------
+func (k msgServer) SetOrchestratorAddresses(c context.Context, msg *types.MsgSetOrchestratorAddresses) (*types.MsgSetOrchestratorAddressesResponse, error) {
+	c, doneFn := metrics.ReportFuncCallAndTimingCtx(c, k.svcTags)
+	defer doneFn()
+
+	ctx := sdk.UnwrapSDKContext(c)
+	validatorAccountAddr, _ := sdk.AccAddressFromBech32(msg.Sender)
+	validatorAddr := sdk.ValAddress(validatorAccountAddr.Bytes())
+
+	// get orchestrator address if available. otherwise default to validator address.
+	var orchestratorAddr sdk.AccAddress
+	if msg.Orchestrator != "" {
+		orchestratorAddr, _ = sdk.AccAddressFromBech32(msg.Orchestrator)
+	} else {
+		orchestratorAddr = validatorAccountAddr
+	}
+
+	valAddr, foundExistingOrchestratorKey := k.Keeper.GetOrchestratorValidator(ctx, msg.HyperionId, orchestratorAddr)
+	ethAddress, foundExistingEthAddress := k.Keeper.GetEthAddressByValidator(ctx, msg.HyperionId, validatorAddr)
+	fmt.Println("valAddr: ", valAddr)
+	fmt.Println("orchestratorAddr: ", orchestratorAddr)
+	fmt.Println("ethAddress: ", ethAddress)
+
+	// ensure that the validator exists
+	if val, err := k.Keeper.StakingKeeper.Validator(ctx, validatorAddr); err != nil || val == nil {
+		if err == nil {
+			err = stakingtypes.ErrNoValidatorFound
+		}
+		metrics.ReportFuncError(k.svcTags)
+		return nil, errors.Wrap(err, validatorAddr.String())
+	} else if foundExistingOrchestratorKey || foundExistingEthAddress {
+		metrics.ReportFuncError(k.svcTags)
+		return nil, errors.Wrap(types.ErrResetDelegateKeys, validatorAddr.String())
+	}
+
+	// set the orchestrator address
+	k.Keeper.SetOrchestratorValidator(ctx, msg.HyperionId, validatorAddr, orchestratorAddr)
+	// set the ethereum address
+	fmt.Println("msg.EthAddress: ", msg.EthAddress)
+	ethAddr := common.HexToAddress(msg.EthAddress)
+	fmt.Println("ethAddr: ", ethAddr)
+	k.Keeper.SetEthAddressForValidator(ctx, msg.HyperionId, validatorAddr, ethAddr)
+
+	// nolint:errcheck //ignored on purpose
+	ctx.EventManager().EmitTypedEvent(&types.EventSetOrchestratorAddresses{
+		ValidatorAddress:    validatorAddr.String(),
+		OrchestratorAddress: orchestratorAddr.String(),
+		OperatorEthAddress:  msg.EthAddress,
+		HyperionId:          msg.HyperionId,
+	})
+	fmt.Println("SetOrchestratorAddresses success")
+
+	return &types.MsgSetOrchestratorAddressesResponse{}, nil
+
+}
+
+// [Not Used In Hyperion] AddCounterpartyChainParams msgServer allows adding connectivity for a new blockchain.
+// -------------
+// MsgAddCounterpartyChainParams
+// For this, a new HyperionId and a new BridgeChainId are required.
+// The function validates the counterparty chain parameters provided in the message,
+// checks that the HyperionId and BridgeChainId are not already in use to avoid duplicates,
+// and if the validations are successful, it adds the new counterparty chain parameters
+// to the existing parameters and saves them.
+// TODO: using it via proposal and check it well approved before adds.
+// -------------
+func (k msgServer) AddCounterpartyChainParams(c context.Context, msg *types.MsgAddCounterpartyChainParams) (*types.MsgAddCounterpartyChainParamsResponse, error) {
+	c, doneFn := metrics.ReportFuncCallAndTimingCtx(c, k.svcTags)
+	defer doneFn()
+
+	ctx := sdk.UnwrapSDKContext(c)
+
+	k.Keeper.Logger(ctx).Info("AddCounterpartyChainParams -1")
+	// todo check msg.orchestrator funds and pay the cost of AddCounterpartyChain to the fundation
+
+	if err := msg.CounterpartyChainParams.ValidateBasic(); err != nil {
+		return nil, err
+	}
+
+	k.Keeper.Logger(ctx).Info("AddCounterpartyChainParams 0")
+	params := k.Keeper.GetParams(ctx)
+
+	k.Keeper.Logger(ctx).Info("AddCounterpartyChainParams 1")
+
+	for _, counterpartyChainParam := range params.CounterpartyChainParams {
+		if counterpartyChainParam.HyperionId == msg.CounterpartyChainParams.HyperionId {
+			return nil, errors.Wrap(types.ErrDuplicate, "HyperionId already exists")
+		}
+		if counterpartyChainParam.BridgeChainId == msg.CounterpartyChainParams.BridgeChainId {
+			return nil, errors.Wrap(types.ErrDuplicate, "BridgeChainId already exists")
+		}
+	}
+
+	k.Keeper.Logger(ctx).Info("AddCounterpartyChainParams 2")
+
+	params.CounterpartyChainParams = append(params.CounterpartyChainParams, msg.CounterpartyChainParams)
+	k.Keeper.SetParams(ctx, params)
+
+	k.Keeper.Logger(ctx).Info("AddCounterpartyChainParams 3")
+
+	return &types.MsgAddCounterpartyChainParamsResponse{}, nil
+}
+
+// [Not Used In Hyperion] CancelSendToChain
+// -------------
+// MsgCancelSendToChain permit to cancel send
+// to chain if the sendtochain is always in the tx pool.
+// -------------
+func (k msgServer) CancelSendToChain(c context.Context, msg *types.MsgCancelSendToChain) (*types.MsgCancelSendToChainResponse, error) {
+	// c, doneFn := metrics.ReportFuncCallAndTimingCtx(c, k.svcTags)
+	// defer doneFn()
+
+	ctx := sdk.UnwrapSDKContext(c)
+	sender, err := sdk.AccAddressFromBech32(msg.Sender)
+	if err != nil {
+		metrics.ReportFuncError(k.svcTags)
+		return nil, err
+	}
+
+	err = k.Keeper.RemoveFromOutgoingPoolAndRefund(ctx, msg.TransactionId, sender)
+	if err != nil {
+		metrics.ReportFuncError(k.svcTags)
+		return nil, err
+	}
+
+	// nolint:errcheck //ignored on purpose
+	ctx.EventManager().EmitTypedEvent(&types.EventCancelSendToChain{
+		OutgoingTxId: msg.TransactionId,
+	})
+
+	return &types.MsgCancelSendToChainResponse{}, nil
+}
+
+// [Not Used In Hyperion] SubmitBadSignatureEvidence
+// -------------
+// MsgSubmitBadSignatureEvidence
+// This call allows anyone to submit evidence
+// that a validator has signed a valset, batch,
+// or logic call that never existed. Subject
+// contains the batch, valset, or logic call.
+// -------------
+func (k msgServer) SubmitBadSignatureEvidence(c context.Context, msg *types.MsgSubmitBadSignatureEvidence) (*types.MsgSubmitBadSignatureEvidenceResponse, error) {
+	c, doneFn := metrics.ReportFuncCallAndTimingCtx(c, k.svcTags)
+	defer doneFn()
+
+	ctx := sdk.UnwrapSDKContext(c)
+	err := k.Keeper.CheckBadSignatureEvidence(ctx, msg)
+
+	// nolint:errcheck //ignored on purpose
+	ctx.EventManager().EmitTypedEvent(&types.EventSubmitBadSignatureEvidence{
+		BadEthSignature:        msg.Signature,
+		BadEthSignatureSubject: msg.Subject.String(),
+	})
+
+	if err != nil {
+		metrics.ReportFuncError(k.svcTags)
+	}
+
+	return &types.MsgSubmitBadSignatureEvidenceResponse{}, err
+}
+
+// [Not Used In Hyperion] UpdateParams
+// -------------
+// MsgUpdateParams
+// This call permit to change in one time all the params.
+// TODO: remove this call not good to have an authority who can touch alone the params.
+// -------------
+func (k msgServer) UpdateParams(c context.Context, msg *types.MsgUpdateParams) (*types.MsgUpdateParamsResponse, error) {
+	c, doneFn := metrics.ReportFuncCallAndTimingCtx(c, k.svcTags)
+	defer doneFn()
+
+	if msg.Authority != k.Keeper.authority {
+		return nil, errors.Wrapf(govtypes.ErrInvalidSigner, "invalid authority: expected %s, got %s", k.Keeper.authority, msg.Authority)
+	}
+
+	if err := msg.Params.ValidateBasic(); err != nil {
+		return nil, err
+	}
+
+	ctx := sdk.UnwrapSDKContext(c)
+	k.Keeper.SetParams(ctx, &msg.Params)
+
+	return &types.MsgUpdateParamsResponse{}, nil
+}
+
+// [Not Used In Hyperion] BlacklistEthereumAddresses
+// -------------
+// MsgBlacklistEthereumAddresses
+// Defines the message used to add Ethereum addresses to all hyperion blacklists.
+// TODO: adding this call on proposals and remove authority
+// -------------
+func (k msgServer) BlacklistEthereumAddresses(ctx context.Context, msg *types.MsgBlacklistEthereumAddresses) (*types.MsgBlacklistEthereumAddressesResponse, error) {
+	defer metrics.ReportFuncCallAndTiming(k.svcTags)()
+
+	sdkContext := sdk.UnwrapSDKContext(ctx)
+
+	isValidSigner := k.Keeper.authority == msg.Signer || k.Keeper.isAdmin(sdkContext, msg.Signer)
+	if !isValidSigner {
+		return nil, errors.Wrapf(govtypes.ErrInvalidSigner, "the signer %s is not the valid authority or one of the Hyperion module admins", msg.Signer)
+	}
+
+	for _, address := range msg.BlacklistAddresses {
+		blacklistAddr, err := types.NewEthAddress(address)
+		if err != nil {
+			return nil, errors.Wrapf(err, "invalid blacklist address %s", address)
+		}
+		k.Keeper.SetEthereumBlacklistAddress(sdkContext, *blacklistAddr)
+	}
+
+	return &types.MsgBlacklistEthereumAddressesResponse{}, nil
+}
+
+// [Not Used In Hyperion] RevokeEthereumBlacklist
+// -------------
+// MsgRevokeEthereumBlacklist
+// Defines the message used to remove Ethereum addresses from hyperion blacklist.
+// TODO: adding this call on proposals and remove authority
+// -------------
+func (k msgServer) RevokeEthereumBlacklist(ctx context.Context, msg *types.MsgRevokeEthereumBlacklist) (*types.MsgRevokeEthereumBlacklistResponse, error) {
+	defer metrics.ReportFuncCallAndTiming(k.svcTags)()
+
+	sdkContext := sdk.UnwrapSDKContext(ctx)
+
+	isValidSigner := k.Keeper.authority == msg.Signer || k.Keeper.isAdmin(sdkContext, msg.Signer)
+	if !isValidSigner {
+		return nil, errors.Wrapf(govtypes.ErrInvalidSigner, "the signer %s is not the valid authority or one of the Hyperion module admins", msg.Signer)
+	}
+
+	for _, blacklistAddress := range msg.BlacklistAddresses {
+
+		blacklistAddr, err := types.NewEthAddress(blacklistAddress)
+		if err != nil {
+			return nil, errors.Wrapf(err, "invalid blacklist address %s", blacklistAddress)
+		}
+
+		if !k.Keeper.IsOnBlacklist(sdkContext, *blacklistAddr) {
+			return nil, fmt.Errorf("invalid blacklist address")
+		} else {
+			k.Keeper.DeleteEthereumBlacklistAddress(sdkContext, *blacklistAddr)
+		}
+	}
+
+	return &types.MsgRevokeEthereumBlacklistResponse{}, nil
+}
