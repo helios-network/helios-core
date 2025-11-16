245b2ef6d981b7d76d9a3fa462f31c70cd9ea8ee
jguyet <jguyet.weoohh@gmail.com>
2025-11-16 01:25:45 +0100

    Update release version to v0.0.271, adjust cosmos-sdk dependencies to v0.50.10-helios-158, and add new API endpoint for querying all skipped nonces.

--- create-release.sh
+++ create-release.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Config
 REPO="helios-network/helios-core"           # ex: "mon-org/mon-repo"
-TAG="v0.0.254"
+TAG="v0.0.271"
 TITLE="Release ${TAG}"
 NOTES="Release of the Helios Core"
 BINARY_PATH="/Users/jeremyguyet/go/bin/heliades"  # chemin vers ton binaire
